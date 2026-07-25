170280bff535a6cc1d4c6723e8346e82370ac9b6
Michael Hughes <microneesia79@gmail.com>
2026-07-25 15:56:37 -0700

    Wire authentication and family providers

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,2 +1,19 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import { BrowserRouter } from 'react-router-dom';import App from './App';import './styles.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import {AuthProvider} from './auth/AuthContext';
+import {FamilyProvider} from './family/FamilyContext';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <FamilyProvider>
+          <App />
+        </FamilyProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
